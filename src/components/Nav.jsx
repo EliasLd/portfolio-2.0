ddcc82986ba5df61bf8dd9840e323ae8d78b094f
Elias <elabd.elias@gmail.com>
2024-03-29 23:25:51 +0100

    added animation hovering links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -16,5 +16,5 @@ export default function Nav() {
             </div>
         </nav>
     </div>
-  )
+  );
 }
--- src/components/Pres.jsx
+++ src/components/Pres.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import { useState } from 'react';
+import { Animate } from 'react-simple-animate';
+
 import { TypeAnimation } from 'react-type-animation';
+import { FaLinkedin } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { MdOutlineFileDownload } from "react-icons/md";
 import flat from '../assets/personFlat.svg';
 
 export default function Pres() {
+
+  const [hovered, setHovered] = useState(null);
+
   return (
     <div className='flex justify-center mt-10'>
-        <div className='flex flex-col gap-y-6'>
+        <div className='flex flex-col gap-y-8'>
             <div className='text-xl font-sans font-bold text-blue-300'>
                 <TypeAnimation
                     sequence={[
-                      "Bonjour, moi c'est Elias !",
-                      500, 
                       "Hi, I'm Elias !",
+                      500, 
+                      "Moi c'est Elias !",
                       1000,
                       '私はエリアスです !',
                       1000,
@@ -29,10 +38,45 @@ export default function Pres() {
             </div>
             <div className='flex justify-center'>
                 <div className='px-3 rounded-full bg-sky-900'>
-                    <img src={flat} className='w-70 h-70'></img>
+                    <img src={flat} alt='' className='w-70 h-70'></img>
+                </div>
+            </div>
+            <div className='flex flex-row justify-center gap-x-12 relative'>
+                <div className='flex flex-col items-center'>
+                    <FaLinkedin 
+                        size={30} color='white' className='cursor-pointer' 
+                        onMouseEnter={() => setHovered('LinkedIn')} onMouseLeave={() => setHovered(null)} 
+                    />
+                    {hovered === 'LinkedIn' && (
+                        <div className='absolute top-0 mt-16 text-white text-sm slide-top bg-sky-700 px-2 py-1 rounded-full'>
+                            <p className='text-sm font-inter text-slate-100'>LinkedIn</p>
+                        </div>
+                    )}
+                </div>
+                <div className='flex flex-col items-center'>
+                    <FaGithub 
+                        size={30} color='white' className='cursor-pointer'
+                        onMouseEnter={() => setHovered('GitHub')} onMouseLeave={() => setHovered(null)} 
+                    />
+                    {hovered === 'GitHub' && (
+                        <div className='absolute top-0 mt-16 text-white text-sm slide-top bg-sky-700 px-2 py-1 rounded-full'>
+                            <p className='text-sm font-inter text-slate-100'>GitHub</p>
+                        </div>
+                    )}
+                </div>
+                <div className='flex flex-col items-center'>
+                    <MdOutlineFileDownload 
+                        size={30} color='white' className='cursor-pointer'
+                        onMouseEnter={() => setHovered('Download')} onMouseLeave={() => setHovered(null)} 
+                    />
+                    {hovered === 'Download' && (
+                        <div className='absolute top-0 mt-16 text-white text-sm slide-top bg-sky-700 px-2 py-1 rounded-full'>
+                            <p className='text-sm font-inter text-slate-100'>CV</p>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
     </div>
-  )
+  );
 }
