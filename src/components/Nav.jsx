f732c838df5940cfcba107c5038893a7751be471
Elias <elabd.elias@gmail.com>
2024-03-31 21:43:32 +0200

    fixed nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,6 +3,7 @@ import { motion, useAnimation, useAnimate, stagger, } from "framer-motion";
 import { useEffect, useState } from "react";
 
 import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross1 } from "react-icons/rx";
 
 export default function Nav() {
 
@@ -43,12 +44,12 @@ export default function Nav() {
         <div className='mx-w-5xl mx-auto px-4 ' ref={scope}>
             <div className='flex items-center justify-between h-16'>
               <motion.button onClick={() => setOpen(!open)} whileTap={{scale: 0.70, opacity: 0.75}}>
-                <RxHamburgerMenu size={35} color='white'/>
+                {open ?  <RxCross1 size={35} color='white'/> : <RxHamburgerMenu size={35} color='white'/>}
               </motion.button>
               <ul className='flex flex-col gap-y-3 justify-center items-center mt-52 rounded-3xl bg-slate-100 text-black p-2 mr-5'>
                 {items.map((item, i) => (
-                  <div key={`${i}-D`} className='bg-slate-300 rounded-full w-3/4 flex justify-center'>
-                    <motion.li key={i} className='mx-4 font-inter font-semibold text-sm'>{item}</motion.li>
+                  <div key={`${i}-D`} className='bg-slate-300 rounded-full w-3/4 flex justify-center cursor-pointer transition ease-in-out duration-300 hover:scale-105'>
+                    <motion.li key={i} className='mx-4 font-inter font-bold text-sm'>{item}</motion.li>
                   </div>
                 ))}           
               </ul>
