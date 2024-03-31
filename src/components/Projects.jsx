416141da7adb577ff8e6eabcdb02913dcdbb639e
Elias <elabd.elias@gmail.com>
2024-03-31 18:59:53 +0200

    added animation to project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,8 +8,8 @@ import { useEffect } from "react";
 export default function Projects() {
 
   const appearing = {
-    visible: { opacity: 1, transition: { duration: 0.5} },
-    hidden: { opacity: 0, },
+    visible: { opacity: 1, y:0,  transition: { duration: 0.5} },
+    hidden: { opacity: 0 , y:100},
   };
 
   const ctrl = useAnimation();
@@ -28,7 +28,8 @@ export default function Projects() {
       <div className='mb-16'>
         <p className='font-sans font-bold text-white text-2xl'>My Projects</p>
       </div>
-      <motion.div ref={ref} variants={appearing} initial="hidden" animate={ctrl} className='flex justify-center'>
+      <motion.div 
+        ref={ref} variants={appearing} initial="hidden" animate={ctrl} className='flex justify-center'>
         <div className='grid grid-cols-1 lg:grid-cols-2 p-4 gap-2 bg-slate-700 relative lg:w-144 sm:w-128 rounded-3xl mx-2 sm:mx-0'>
             {projects.map((project, i) => (
               <div className='m-1 sm:m-5'>
