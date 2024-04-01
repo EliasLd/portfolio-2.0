8107a842905d5377a2f61dced699e19ec01df940
Elias <elabd.elias@gmail.com>
2024-04-01 16:30:50 +0200

    added text animation

--- src/components/Desc.jsx
+++ src/components/Desc.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import '../index.css';
 
 export default function Desc() {
   return (
     <div className='p-3 flex flex-col justify-center items-center mt-20'>
       <div className='flex justify-center'>
-        <h1 className='font-sans font-bold text-white text-2xl'>Trying to build useful things.</h1>
+        <h1 className='font-sans font-bold text-white text-2xl text-focus-in'>Trying to build useful things.</h1>
       </div>
       <div className='p-4 flex  mt-5'>
           <p className='font-inter font-semibold text-md text-slate-400'>Currently following an engineering <br/> program at 
